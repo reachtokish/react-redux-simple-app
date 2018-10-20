154cbb042648bdc997cfcc221b1ac76321893afe
Kishore Patra <kishorekumar@nodetech.in>
2018-10-20 11:57:24 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
-import UserList from './components/user-list';
-import ActiveUser from './components/user-active';
 
 class App extends Component {
 	
 	render() {
 		return (
 			<div className="App">
-				<h1>Hey</h1>
-				
+				<h2>Add User</h2>
+				<table>
+					<tbody>
+						<tr>
+							<th>
+								Name
+							</th>
+							<td>
+								<input type="text" />
+							</td>
+						</tr>
+						<tr>
+							<th>
+								Phone
+							</th>
+							<td>
+								<input type="text" />
+							</td>
+						</tr>
+						<tr>
+							<th>&nbsp;</th>
+							<td>
+								<button type="button">Add User</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<hr />
+				<h2>User List</h2>
+				<table cellSpacing="0" cellPadding="5" border="1">
+					<tbody>
+						<tr>
+							<th>Name</th>
+							<th>Phone</th>
+						</tr>
+						<tr>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		);
 	}
